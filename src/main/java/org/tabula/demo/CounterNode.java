97459c85502bfc0269bfcbb38e26574792077dea
bo_le <boleerandersen@gmail.com>
2024-05-10 13:50:32 +0200

    Cleanup

--- src/main/java/org/tabula/demo/CounterNode.java
+++ src/main/java/org/tabula/demo/CounterNode.java
@@ -14,7 +14,6 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.util.Objects;
